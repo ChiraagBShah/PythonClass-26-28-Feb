2d4d07cb7e61b0792cbd36dd68d0d7578ccd3419
Shah <chirag.i.shah@capgemini.com>
2018-02-28 14:23:51 +0530

    Initial commit

---
+++ Tuples.py
@@ -0,0 +1,9 @@
+tuple = ['chirag', 786, 2.24, 'shah', 78.4 ]
+tinytuple = [123, 'bnpp']
+
+print(tuple)
+print(tuple[0])
+print(tuple[1:3])
+print(tuple[2:])
+print(tinytuple*2)
+print(tuple+tinytuple)
